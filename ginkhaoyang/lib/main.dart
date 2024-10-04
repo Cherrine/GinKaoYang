4ecd30b82c13f79b56a3675f30878cfb6840528a
Cherrine <66070031@kmitl.ac.th>
2024-10-05 00:03:08 +0700

    Update login page (edit later)

---
+++ ginkhaoyang/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/login.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
