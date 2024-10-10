08c7417b272e5fd3a201f1fb71fc5d4fd34a4961
KawaiiZT <Trisit.93@gmail.com>
2024-10-10 22:33:43 +0700

    Add firebase

--- ginkhaoyang/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ginkhaoyang/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_functions
+import firebase_auth
+import firebase_core
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
