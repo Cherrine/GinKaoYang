4735d100524926b995eed9d8bd03aee76dec21b1
Cherrine <66070031@kmitl.ac.th>
2024-10-17 18:03:43 +0700

    Redo

--- ginkhaoyang/android/app/src/main/kotlin/com/example/ginkhaoyang/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ginkhaoyang
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ginkhaoyang/lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ginkhaoyang/controllers/menu_controller.dart';
-import 'package:ginkhaoyang/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
